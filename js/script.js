a160633ac9af37e8e2278b4028e09d466e89541a
Nicolas Garcia <100208523+ProjetosNicolasGarcia@users.noreply.github.com>
2023-02-28 11:07:56 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+function troca0(){
+    window.location.href = "index.html"
+
+}
+
+function troca(){
+
+    window.location.href = "sobre.html"
+}
+
+function troca1(){
+    window.location.href = "ingredientes.html"
+}
+
+function troca2(){
+    window.location.href = "cardapio.html"
+}
+
+function troca3(){
+    window.location.href = "contato.html"
+}
+
+
+
+
